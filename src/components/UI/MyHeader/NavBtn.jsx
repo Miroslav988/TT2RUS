b20b80955a8f2281e8521e9f8863a58c86193451
Miroslav <mir.vbok@mail.ru>
2024-10-05 00:09:04 +0700

    --

--- src/components/UI/MyHeader/NavBtn.jsx
+++ src/components/UI/MyHeader/NavBtn.jsx
@@ -3,7 +3,7 @@ import cl from "./HeaderStyles.module.css";
 const NavBtn = ({ children }) => {
   return (
     <li className={cl.navBtns} type="button">
-      <a href="#">{children}</a>
+      <a>{children}</a>
     </li>
   );
 };
